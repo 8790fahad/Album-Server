e61de1563af7e0cfe9bce8af326be1a7fa7abd51
8790fahad <8790fahadado@gmail.com>
2024-03-09 17:49:13 +0100

    cgan

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,32 @@
-'use strict';
+"use strict";
 
-import fs  from 'fs';
-import path  from 'path';
-import Sequelize  from 'sequelize';
-require('dotenv').config();
+import fs from "fs";
+import path from "path";
+import Sequelize from "sequelize";
+require("dotenv").config();
 
 const basename = path.basename(__filename);
 
+// const config = {
+//   username: "root",
+//   password: "",
+//   database: "album",
+//   host: "localhost",
+//   dialect: "mysql",
+//   port:3307,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
 const config = {
-  username: "root",
-  password: "",
-  database: "album",
-  host: "localhost",
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   dialect: "mysql",
-  port:3307,
   pool: {
     max: 5,
     min: 0,
@@ -28,20 +41,26 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+  .forEach((file) => {
+    const model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
